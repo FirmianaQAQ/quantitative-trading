d6d935d460c4d3c3a9bba460d775704e199c9315
yangguangyu <yangguangyu@21tb.com>
2026-06-18 17:00:59 +0800

    :art: edit

--- tests/test_strategy_registry.py
+++ tests/test_strategy_registry.py
@@ -1,6 +1,10 @@
 import unittest
 
-from backtest.strategy_registry import get_default_strategy_spec, list_strategy_specs
+from backtest.strategy_registry import (
+    get_default_strategy_spec,
+    group_strategy_specs,
+    list_strategy_specs,
+)
 from utils.default_stocks import (
     DEFAULT_BASE_STRATEGY_ID,
     DEFAULT_BASE_STRATEGY_MODULE_NAME,
@@ -31,6 +35,19 @@ class DefaultStrategySourceTests(unittest.TestCase):
         self.assertEqual(spec.strategy_id, DEFAULT_BASE_STRATEGY_ID)
         self.assertEqual(spec.module_name, DEFAULT_BASE_STRATEGY_MODULE_NAME)
         self.assertEqual(spec.display_name, DEFAULT_BASE_STRATEGY_NAME)
+
+    def test_registry_exposes_mas_as_top_level_family(self) -> None:
+        list_strategy_specs.cache_clear()
+
+        grouped_specs = group_strategy_specs()
+        family_names = [family_name for family_name, _ in grouped_specs]
+
+        self.assertGreaterEqual(len(family_names), 2)
+        self.assertEqual(family_names[:2], [DEFAULT_BASE_STRATEGY_NAME, "MAS"])
+        mas_specs = grouped_specs[1][1]
+        self.assertEqual(len(mas_specs), 1)
+        self.assertEqual(mas_specs[0].strategy_id, "MAS")
+        self.assertEqual(mas_specs[0].family_id, "MAS")
 
 
 if __name__ == "__main__":
